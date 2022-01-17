2ab44ad72a9ac27bb718a8770b7dc48f162af20d
TaherNaqi <eng-naqi@hotmail.com>
2022-01-17 15:20:40 +0300

    Finished with update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import ChatRoom from './components/ChatRoom';
-import ChatRoomsList from './components/ChatRoomsList';
-import { Route, Switch } from 'react-router';
-
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ChatRoom from "./components/ChatRoom";
+import ChatRoomsList from "./components/ChatRoomsList";
+import { Route, Switch } from "react-router";
+import axios from "axios";
 function App() {
+  const [rooms, setRooms] = useState([]);
+  console.log("🚀 ~ file: App.js ~ line 10 ~ App ~ rooms", rooms);
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+  const fetchRooms = async () => {
+    try {
+      const response = await axios.get(
+        "https://coded-task-axios-be.herokuapp.com/rooms"
+      );
+      setRooms(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const [rooms, setRooms] = useState([])
-
-
-
-  const createRoom = (newRoom) => {
+  const createRoom = async (newRoom) => {
     // to do : call BE to create a room
-  }
-
-  const deleteRoom = (id) => {
+    try {
+      const response = await axios.post(
+        "https://coded-task-axios-be.herokuapp.com/rooms",
+        newRoom
+      );
+      setRooms([...rooms, response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const updateRoom = async (room) => {
+    try {
+      await axios.put(
+        `https://coded-task-axios-be.herokuapp.com/rooms/${room.id}`,
+        room
+      );
+      setRooms(rooms.map((ro) => (ro.id === room.id ? (ro = room) : ro)));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const deleteRoom = async (id) => {
     // to do : call BE to delete a room
-  }
+    try {
+      await axios.delete(
+        `http://coded-task-axios-be.herokuapp.com/rooms/${id}`
+      );
+      let tempRooms = rooms.filter((room) => room.id !== id);
+      setRooms(tempRooms);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="__main">
@@ -28,7 +67,12 @@ function App() {
           </Route>
           <Route exact path="/">
             <center>
-              <ChatRoomsList rooms={rooms} />
+              <ChatRoomsList
+                rooms={rooms}
+                createRoom={createRoom}
+                deleteRoom={deleteRoom}
+                updateRoom={updateRoom}
+              />
             </center>
           </Route>
         </Switch>
